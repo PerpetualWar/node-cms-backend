e9ff88f0c2f7182141840751f4f234f107d93b75
PerpetualWar <perpetualwarr@gmail.com>
2018-01-04 12:45:21 +0100

    code cleaning

--- db/mongoose.js
+++ db/mongoose.js
@@ -6,9 +6,4 @@ mongoose.connect(process.env.MONGODB_URI,
   // { useMongoClient: true } //deprecated in mongoose 5.0.0., already using mongoclient
 );
 
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function() {
-//   console.log('connection open')
-
 module.exports = { mongoose };
--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,19 +1,5 @@
 const { User } = require('./../models/user');
 
-// const authenticate = async (req, res, next) => {
-//   const token = req.header('x-auth');
-//   try {
-//     const user = await User.findByToken(token);
-//     if (!user)
-//       return Promise.reject();
-//     req.user = user;
-//     req.token = token;
-//     next();
-//   } catch (e) {
-//     res.status(401).send();
-//   }
-// };
-
 var authenticate = (req, res, next) => {
   var token = req.header('Authorization');
 
